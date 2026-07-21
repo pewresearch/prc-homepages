13250576800477b75e6f4a31d577294475fe1f00
prcdevgitbot <webdev@pewresearch.org>
2026-07-21 14:54:52 +0000

    See https://github.com/pewresearch/prc-platform/commit/b18ceb7c8c0a631b919e50de2a0e1b2dc88cd93d from refs/tags/v1.12.0

--- prc-homepages.php
+++ prc-homepages.php
@@ -32,34 +32,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! defined( 'DEFAULT_TECHNICAL_CONTACT' ) ) {
-	define( 'DEFAULT_TECHNICAL_CONTACT', 'webdev@pewresearch.org' );
-}
-
 define( 'PRC_HOMEPAGES_FILE', __FILE__ );
 define( 'PRC_HOMEPAGES_DIR', __DIR__ );
 define( 'PRC_HOMEPAGES_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-prc-x-x-x-activator.php
- */
-function activate_plugin() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-activator.php';
-	Plugin_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-prc-x-x-x-deactivator.php
- */
-function deactivate_plugin() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-deactivator.php';
-	Plugin_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, '\PRC\Platform\Homepages\activate_plugin' );
-register_deactivation_hook( __FILE__, '\PRC\Platform\Homepages\deactivate_plugin' );
 
 /**
  * The core plugin class that is used to define the hooks that initialize the various components.
